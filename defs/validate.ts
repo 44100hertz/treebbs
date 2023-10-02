6b572f30efe1cff20e8836f224f98dc7e5ce436d
Sam Pagenkopf <ssaammp@gmail.com>
2023-10-02 00:39:46 -0500

    refactor validation

--- defs/validate.ts
+++ defs/validate.ts
@@ -4,15 +4,15 @@ export const MAX_TEXT_LEN = 4096;
 export function validate(author: string, text: string) {
     if (author.length > MAX_AUTHOR_LEN) {
         return 'author name too long';
-    } else if (author.replace(/\s/g, '').length === 0) {
+    } else if (author.trim().length === 0) {
         return 'empty author name';
-    } else if (author.search('\n') !== -1) {
+    } else if (author.search(/\n+/) !== -1) {
         return 'author name contains newline';
     } else if (text.length > MAX_TEXT_LEN) {
        return 'too long';
-    } else if (text.split('\n').length > 100) {
+    } else if (text.split(/\n+/).length > 100) {
         return 'too many newlines';
-    } else if (text.replace(/\s/g, '').length === 0) {
+    } else if (text.trim().length === 0) {
         return 'empty reply';
     } else {
         return undefined;
